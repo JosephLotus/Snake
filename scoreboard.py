b65f0f51b13ad1a51b5022d8f658fd52ab569841
JosephLotus <78066370+JosephLotus@users.noreply.github.com>
2021-02-26 23:08:56 +0800

    Add files via upload
    I have updated the game to include a high score recording function.

--- scoreboard.py
+++ scoreboard.py
@@ -8,22 +8,26 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
-        self.hideturtle()
-        self.refresh()
-
-    def refresh(self):
-        self.clear()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", move=True, align=ALIGNMENT,
-                   font=FONT)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.refresh()
+        self.update_scoreboard()
 
-    def game_over_text(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", move=True, align=ALIGNMENT,
-                   font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.score))
+        self.score = 0
+        self.update_scoreboard()
